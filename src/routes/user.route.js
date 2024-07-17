909f24ad1f27bce92098396e541569629651db2a
Victor Telles <victortelles18@gmail.com>
2024-07-17 12:04:32 -0300

    Feat:Updating data in the database through the API

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/user.controller')
 route.post("/", userController.create);
 route.get("/", userController.findAll);
 route.get("/:id", userController.findById)
+route.patch("/:id",userController.update)   /* patch so altera 1 campo */
 
 
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -7,9 +7,22 @@ const findAllService = () => User.find();
 
 const findByIdService = (id) => User.findById(id);
 
+const updateService = (
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar,
+    background
+) => User.findOneAndUpdate({ _id: id },
+    { name, username, email, password, avatar, background }
+)
+
 module.exports = {
     createService,
     findAllService,
     findByIdService,
+    updateService
 
 };
