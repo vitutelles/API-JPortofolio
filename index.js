ea3342c1a125722d01d03075cc2e3c65f2d0022f
Victor Telles <victortelles18@gmail.com>
2024-07-17 11:12:00 -0300

    Feat:adding user search by id in the database / and creating a user

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express();
+const express = require('express');
 const connectDatabase = require("./src/database/db")
-
-
 const userRoute = require("./src/routes/user.route");
 
 
+
+
+const app = express();
 const port = 3000;
 
 connectDatabase()
